7bd16bc6dd8fa75d52054ef74137aee5aa18ebaf
KwangwonChoi <rhkddnjs99@hotmail.com>
2018-03-07 18:00:52 +0900

    addLineNBarChart

--- Assets/Scripts/Bar.cs
+++ Assets/Scripts/Bar.cs
@@ -5,6 +5,8 @@ using System.Collections;
 public class Bar : MonoBehaviour {
 
     public Image bar;
+    public Image circle;
+    public Image line;
     public Text label;
     public Text barValue;
 }
--- Assets/Scripts/BarChart.cs
+++ Assets/Scripts/BarChart.cs
@@ -7,31 +7,82 @@ using System.Linq;
 public class BarChart : MonoBehaviour {
 
     public Bar barPrefab;
+
     public int[] inputValues;
-    public string[] labels;
 
     List<Bar> bars = new List<Bar>();
 
-
+    [SerializeField]
+    private float _lineThickness;
     private float _chartHeight;
 
+    [SerializeField]
+    private bool _isLineChart;
+    [SerializeField]
+    private bool _isBarChart;
+    
     private void Start()
     {
-        _chartHeight = Screen.height + GetComponent<RectTransform>().sizeDelta.y;
+        _chartHeight = GetComponent<RectTransform>().sizeDelta.y;
         
         DisplayGraph(inputValues);
-        
     }
 
     void DisplayGraph(int[] vals) {
         int maxValue = vals.Max();
 
+        ExpandChartArea(new Vector2(100, 100));
+
+        Vector2 rectSize = GetComponent<RectTransform>().sizeDelta;
+
         for (int i = 0; i < vals.Length; i++) {
+            //Make Bar & Set it as child of this object.
             Bar newBar = Instantiate(barPrefab) as Bar;
             newBar.transform.SetParent(transform);
-            RectTransform rt = newBar.bar.GetComponent<RectTransform>();
-            float normalizedValue = ((float)vals[i] / (float)maxValue) * 0.95f;
+            if (_isBarChart) {
+                Color clr;
+                clr = newBar.bar.GetComponent<Image>().color;
+                clr.a = 1;
+                newBar.bar.GetComponent<Image>().color = clr;
+                    }
+
+            //Setting bar's height.
+            RectTransform rt = newBar.bar.rectTransform;
+            float normalizedValue = ((float)vals[i] / (float)maxValue) * 0.7f;
             rt.sizeDelta = new Vector2(rt.sizeDelta.x, _chartHeight * normalizedValue);
+
+            bars.Add(newBar);
+            
+            if(i > 10)
+                ExpandChartArea(rectSize);
         }
+        //ExpandChartArea(new Vector2(-100, -100));
+        if(_isLineChart)
+            MakeLines();
     }
+
+    private void MakeLines() {
+
+        for(int i = 1; i < bars.Count; i++)
+        {
+            MakeLine(bars[i-1].circle.transform.position, bars[i].circle.transform.position, i);
+        }
+
+    }
+
+    private void ExpandChartArea(Vector2 rectSize) {
+        GetComponent<RectTransform>().sizeDelta += new Vector2(rectSize.x / 10, 0);
+    }
+
+    private void MakeLine(Vector3 toVector, Vector3 fromVector,int idx) {
+        Vector3 subVector = toVector - fromVector;
+
+        RectTransform line = bars[idx].line.rectTransform;
+        
+        line.sizeDelta = new Vector3(_lineThickness, subVector.magnitude);
+        line.Rotate(new Vector3(0, 0, Mathf.Rad2Deg*Mathf.Atan2(subVector.y,subVector.x) - 90));
+    }
+        
+
+    
 }
